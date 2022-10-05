aac114dee89b84e3b53321dc8ff438c3f138c23f
Marcin Bieniek <marcingim@gmail.com>
2022-10-05 16:12:11 +0200

    Add concerts and seats routes

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ app.listen(8000, () => {
 
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes.js');
+const seatsRoutes = require('./routes/seats.routes.js');
 
 //middleware
 app.use(express.urlencoded ({ extended:false }));
 app.use(express.json());
 app.use(cors());
 app.use('/api/', testimonialsRoutes); 
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
   
 app.use((req, res) => {
     res.status(404).send('404 not found...');
