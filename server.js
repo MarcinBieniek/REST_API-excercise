d4e335619a7a5ea5e0da1482a89c19c7d5f3b63e
Marcin Bieniek <marcingim@gmail.com>
2022-10-07 15:14:52 +0200

    Prepare heroku server

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 //imports
 const express = require('express');
-const uuid = require('uuid').v4;
-const app = express();
-const db = require('./db.js');
 const cors = require('cors');
+const uuid = require('uuid').v4;
+const path = require('path');
+const db = require('./db.js');
 
-//set server
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+const app = express();
 
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -19,10 +16,21 @@ const seatsRoutes = require('./routes/seats.routes.js');
 app.use(express.urlencoded ({ extended:false }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api/', testimonialsRoutes); 
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
-  
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).send('404 not found...');
+});
+
+//set server
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
